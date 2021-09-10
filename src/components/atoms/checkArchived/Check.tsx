bba3a16d696e85b06b66a129e02c78a49330ba90
Claudia Gázquez Alessio <cgazquez@intricom.es>
2021-09-10 10:45:27 +0200

    Check & stories created

---
+++ src/components/atoms/checkArchived/Check.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface CheckProps {   //Interface REQUIRED --> we can declare handle 'controls' (Storybook)
+    checked : boolean
+}
+
+export const Check = ({checked} : CheckProps) => React.createElement(
+    'input',{
+        type : 'checkbox',
+        name : 'archived',
+        disabled : true, 
+        checked : checked
+});
